617294b8385907a0aabb0ef5a2b18c9dcd13d17c
LowHigh <lowhigh.ytb@gmail.com>
2022-10-22 00:30:23 +0200

    Basic site navigation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDVfeIxG1YOikfsXVjBViYsz64LGxm3fe0",
   authDomain: "whereisbrian-82f6e.firebaseapp.com",
@@ -21,60 +18,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-
-createUserWithEmailAndPassword(auth, email, password)
-    .then(user => {
-        const user = userCredential.user;
-        console.log(user);
-    }
-    )
-    .catch(error => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(error);
-    }
-    );
-
-signInWithEmailAndPassword(auth, email, password)
-    .then(user => {
-        const user = userCredential.user;
-        console.log(user);
-    }
-    )
-    .catch(error => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(error);
-    }
-    );
-    
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // User is signed in.
-        const displayName = user.displayName;
-        const email = user.email;
-        const emailVerified = user.emailVerified;
-        const photoURL = user.photoURL;
-        const isAnonymous = user.isAnonymous;
-        const uid = user.uid;
-        const providerData = user.providerData;
-        console.log(user);
-    } else {
-        // User is signed out.
-        // ...
-    }
-}
-);
-
-const functions = require('firebase-functions');
-
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-    .onCreate((snap, context) => {
-      const original = snap.data().original;
-      console.log('Uppercasing', context.params.documentId, original);
-      const uppercase = original.toUpperCase();
-      return snap.ref.set({uppercase}, {merge: true});
-    });
-
-
-connectAuthEmulator(auth, "http://localhost:9099");
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   // The location of the build folder described above
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/js'),
     filename: 'bundle.js'
   },
   // Optional and for development only. This provides the ability to
