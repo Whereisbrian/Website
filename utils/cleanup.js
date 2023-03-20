26da232fd9b1e0987154b16bb83a79d1376e2e92
Ulysse <71192394+Ulyssedev@users.noreply.github.com>
2023-03-20 23:17:56 +0100

    ⚡️ Asynchronous bundle fetching

--- utils/cleanup.js
+++ utils/cleanup.js
@@ -1,25 +1,30 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-// get the file path of the JS file in './dist/_astro'
+// get the directory path containing the JS files
 const dirPath = './dist/_astro/';
-const files = fs.readdirSync(dirPath);
-const jsFile = files.find(file => path.extname(file) === '.js');
-const filePath = path.join(dirPath, jsFile);
 
-console.log('Cleaning up file:', filePath);
+// get all JS files in the directory
+const files = fs.readdirSync(dirPath).filter(file => path.extname(file) === '.js');
 
-// read the contents of the file
-const fileContent = fs.readFileSync(filePath, 'utf8');
+// iterate over each file and perform cleanup
+files.forEach((file) => {
+  const filePath = path.join(dirPath, file);
 
-// extract the removed content and write it to licenses.txt
-const removedContent = fileContent.match(/\/\*\*[\s\S]*?\*\//g).join('\n');
-const licensesFilePath = path.join(dirPath, '../licenses.txt');
-fs.writeFileSync(licensesFilePath, removedContent, 'utf8');
+  console.log('Cleaning up file:', filePath);
 
-// remove all content that starts from `/**` and ends with `*/`
-const modifiedContent = fileContent.replace(/\/\*\*[\s\S]*?\*\//g, '');
+  // read the contents of the file
+  const fileContent = fs.readFileSync(filePath, 'utf8');
 
-// write the modified content back to the file with a comment at the beginning
-const comment = `// Licenses can be found in licenses.txt\n`;
-fs.writeFileSync(filePath, comment + modifiedContent, 'utf8');
+  // extract the removed content and write it to licenses.txt
+  const removedContent = fileContent.match(/\/\*\*[\s\S]*?\*\//g).join('\n');
+  const licensesFilePath = path.join(dirPath, '../licenses.txt');
+  fs.writeFileSync(licensesFilePath, fileContent, { flag: 'a' });
+
+  // remove all content that starts from `/**` and ends with `*/`
+  const modifiedContent = fileContent.replace(/\/\*\*[\s\S]*?\*\//g, '');
+
+  // write the modified content back to the file with a comment at the beginning
+  const comment = `// Licenses can be found in licenses.txt\n`;
+  fs.writeFileSync(filePath, comment + modifiedContent, 'utf8');
+});
