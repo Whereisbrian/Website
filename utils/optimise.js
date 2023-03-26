0034a0880bab6863bcdab7ae5f4c4f411cb6d96d
Ulyssedev <lowhigh.ytb@gmail.com>
2023-03-26 12:46:00 +0200

    🎨 Add console logs with color formatting

--- utils/optimise.js
+++ utils/optimise.js
@@ -13,40 +13,41 @@ const files = fs.readdirSync(assetsPath);
 const newAssetsPath = './src/assets';
 
 files.forEach((file) => {
-// create the source and destination paths
-const srcPath = path.join(assetsPath, file);
-const destPath = path.join(newAssetsPath, file);
+  // create the source and destination paths
+  const srcPath = path.join(assetsPath, file);
+  const destPath = path.join(newAssetsPath, file);
 
-// read the content of the source file
-const fileContent = fs.readFileSync(srcPath);
+  // read the content of the source file
+  const fileContent = fs.readFileSync(srcPath);
 
-// write the content of the source file to the destination file
-fs.writeFileSync(destPath, fileContent);
-console.log(`File ${file} has been copied successfully.`);
+  // write the content of the source file to the destination file
+  fs.writeFileSync(destPath, fileContent);
+  console.log(`[${new Date().toLocaleTimeString()}] ${'\x1b[90m'}File ${file} has been copied successfully.${'\x1b[0m'}`);
 });
 
-console.log('All assets have been copied successfully.');
+console.log(`[${new Date().toLocaleTimeString()} ${'\x1b[34m'}All assets have been copied successfully.${'\x1b[0m'}`);
 
 // read all files in the directory
 fs.readdir(postsPath, (err, files) => {
-    if (err) throw err;
-  
-    // iterate through all files
-    files.forEach(file => {
-      const filePath = path.join(postsPath, file);
-  
-      // check if the file is a markdown file
-      if (path.extname(filePath) === '.md') {
-        // read the file contents
-        const fileContent = fs.readFileSync(filePath, 'utf-8');
-  
-        // replace all occurrences of '/assets/' with '../../assets'
-        const newContent = fileContent.replace(/\/assets\//g, '../../assets/');
-  
-        // write the updated content back to the file
-        fs.writeFileSync(filePath, newContent, 'utf-8');
-      }
-    });
+  if (err) throw err;
+
+  // iterate through all files
+  files.forEach(file => {
+    const filePath = path.join(postsPath, file);
+
+    // check if the file is a markdown file
+    if (path.extname(filePath) === '.md') {
+      // read the file contents
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+
+      // replace all occurrences of '/assets/' with '../../assets'
+      const newContent = fileContent.replace(/\/assets\//g, '../../assets/');
+
+      // write the updated content back to the file
+      fs.writeFileSync(filePath, newContent, 'utf-8');
+      console.log(`[${new Date().toLocaleTimeString()}] ${'\x1b[90m'}File ${filePath} has been updated successfully.${'\x1b[0m'}`);
+    }
+  });
 });
 
-console.log('All posts have been updated successfully.');
+console.log(`[${new Date().toLocaleTimeString()}] ${'\x1b[34m'}All posts have been updated successfully.${'\x1b[0m'}`);
