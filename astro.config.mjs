13a1f74ede01d5be90235c868693f783a6b7eaf3
Ulysse <71192394+Ulyssedev@users.noreply.github.com>
2023-04-15 17:12:59 +0200

    🎉 Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
     assets: true
   },
   site: 'https://whereisbrian.org',
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), partytown()],
   vite: {
     server: {
       watch: {
