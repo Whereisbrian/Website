b970f1898c9921ce9124beec869a47871552ffdc
Ulysse <71192394+Ulyssedev@users.noreply.github.com>
2023-04-18 12:46:40 +0200

    💫 Improve page transition

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client-image" />
-declare module 'astro-spa';
-declare module 'astro:content';
+declare module "astro-spa";
+declare module "astro:content";
--- utils/cleanup.js
+++ utils/cleanup.js
@@ -1,40 +1,46 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
 
 // get the directory path containing the JS files
-const dirPath = './dist/_astro/';
+const dirPath = "./dist/_astro/";
 
 // get all JS files in the directory
-const files = fs.readdirSync(dirPath).filter(file => path.extname(file) === '.js');
+const files = fs
+  .readdirSync(dirPath)
+  .filter((file) => path.extname(file) === ".js");
 
 // iterate over each file and perform cleanup
 files.forEach((file) => {
   const filePath = path.join(dirPath, file);
 
   // console log cleanup process
-  console.log(`[${new Date().toLocaleTimeString()}] \x1b[34mCleaning up file:\x1b[0m ${filePath}`);
+  console.log(
+    `[${new Date().toLocaleTimeString()}] \x1b[34mCleaning up file:\x1b[0m ${filePath}`
+  );
 
   // read the contents of the file
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const fileContent = fs.readFileSync(filePath, "utf8");
 
   // extract the removed content and write it to licenses.txt
   let removedContent;
 
   try {
-    removedContent = fileContent.match(/\/\*\*[\s\S]*?\*\//g).join('\n');
+    removedContent = fileContent.match(/\/\*\*[\s\S]*?\*\//g).join("\n");
   } catch (error) {
-    console.error('The file does not contain any license information.');
+    console.error("The file does not contain any license information.");
   }
-  const licensesFilePath = path.join(dirPath, '../licenses.txt');
-  fs.writeFileSync(licensesFilePath, fileContent, { flag: 'a' });
+  const licensesFilePath = path.join(dirPath, "../licenses.txt");
+  fs.writeFileSync(licensesFilePath, fileContent, { flag: "a" });
 
   // remove all content that starts from `/**` and ends with `*/`
-  const modifiedContent = fileContent.replace(/\/\*\*[\s\S]*?\*\//g, '');
+  const modifiedContent = fileContent.replace(/\/\*\*[\s\S]*?\*\//g, "");
 
   // write the modified content back to the file with a comment at the beginning
   const comment = `// Licenses can be found in licenses.txt\n`;
-  fs.writeFileSync(filePath, comment + modifiedContent, 'utf8');
+  fs.writeFileSync(filePath, comment + modifiedContent, "utf8");
 });
 
 // console log completion message
-console.log(`[${new Date().toLocaleTimeString()}] \x1b[32mCleanup completed successfully.\x1b[0m`);
+console.log(
+  `[${new Date().toLocaleTimeString()}] \x1b[32mCleanup completed successfully.\x1b[0m`
+);
